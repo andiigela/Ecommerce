6db53bc45e8ddcb903b74920cff87c0ec27ea4e6
andiigela <109698122+andiigela@users.noreply.github.com>
2023-11-21 20:00:41 +0100

    feature/Products-Page: Created cart details component to show products added in basket

---
+++ sales-app/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {CartService} from "../../services/cart.service";
+import {CartItem} from "../../common/cart-item";
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit{
+  cartItems: CartItem[] = [];
+  totalQuantity: number = 0;
+  totalPrices: number = 0;
+  constructor(private cartService: CartService) {
+  }
+
+  ngOnInit(): void {
+    this.listOfProducts();
+  }
+  listOfProducts(){
+    this.cartItems = this.cartService.cartItems;
+    this.cartService.totalQuantity.subscribe(data => this.totalQuantity=data);
+    this.cartService.totalPrice.subscribe(data => this.totalPrices=data);
+    this.cartService.computeCartTotals();
+  }
+
+}
