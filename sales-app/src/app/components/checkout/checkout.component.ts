491f4f99d319e4dfa2c69d8d2e81d67827c0c20a
andiigela <andiigela@gmail.com>
2023-11-28 15:09:16 +0100

    feature/Products-Page: Added basic validations for some inputs in my form

--- sales-app/src/app/components/checkout/checkout.component.ts
+++ sales-app/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ShopformService} from "../../services/shopform.service";
 import {start} from "@popperjs/core";
 import {Country} from "../../common/country";
@@ -26,9 +26,9 @@ export class CheckoutComponent implements OnInit{
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
         customer: this.formBuilder.group({
-            firstName: '',
-            lastName: '',
-            email: '',
+            firstName: new FormControl('',[Validators.required,Validators.minLength(2)]),
+            lastName: new FormControl('',[Validators.required,Validators.minLength(2)]),
+            email: new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
         }),
         shippingAddress: this.formBuilder.group({
             street: '',
@@ -59,8 +59,16 @@ export class CheckoutComponent implements OnInit{
 
     this.shopForm.retrieveCountries().subscribe(countries => this.countries=countries);
   }
+  get firstName(){return this.checkoutFormGroup.get('customer.firstName');}
+  get lastName(){return this.checkoutFormGroup.get('customer.lastName');}
+  get email(){return this.checkoutFormGroup.get('customer.email');}
+
   onSubmit(){
     console.log('Handling the submit button');
+    if(this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
     console.log(this.checkoutFormGroup.get('shippingAddress')?.value.zipCode);
     console.log(`Shipping Address Country is: ${this.checkoutFormGroup.get('shippingAddress')?.value.country.name}`);
     console.log(`Billing Address Country is: ${this.checkoutFormGroup.get('billingAddress')?.value.country.name}`);
@@ -94,13 +102,15 @@ export class CheckoutComponent implements OnInit{
       data=>{
         if(formGroupName === 'shippingAddress'){
           this.shippingAddressStates = data;
-            this.checkoutFormGroup.get('shippingAddress')?.get('state')?.setValue(data[0]);
+          this.checkoutFormGroup.get('shippingAddress')?.get('state')?.setValue(data[0]);
         } else {
           this.billingAddressStates = data;
           this.checkoutFormGroup.get('billingAddress')?.get('state')?.setValue(data[0]);
         }
       }
     );
+
+
 
 
   }
